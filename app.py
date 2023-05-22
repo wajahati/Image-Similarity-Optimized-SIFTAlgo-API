b770a4a7267e2364c021951996d42e443ec3d0bc
Wajahat Ahmad <67877858+wajahati@users.noreply.github.com>
2023-05-22 18:38:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 14 02:11:08 2023
-
-@author: mr.laptop
-"""
-
-# 1. Library imports
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from API import Similarity
-
-
 import cv2
 import numpy as np
 import gc
@@ -21,46 +11,34 @@ from PIL import Image
 
 app = FastAPI()
 
-def compare_images(img1, img2):
-    sift = cv2.SIFT_create()
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+def download_image(url, max_size=800):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img.thumbnail((max_size, max_size), Image.ANTIALIAS)
+    return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
-
-    good_matches = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good_matches.append([m])
-
-    return len(good_matches)
-
-
-def are_images_similar(inp_imgs, pro_urls):
-    threshold = 50
-    similar_images = []
-    for img1 in inp_imgs:
-        for url2 in pro_urls:
-            img2 = cv2.imread(url2)
-            num_good_matches = compare_images(img1, img2)
-            if num_good_matches > threshold:
-                similar_images.append((img1, url2))
-                break
+def are_images_identical(url_list1, url_list2):
+    identical_images = []
+    for url1 in url_list1:
+        img1 = download_image(url1)
+        for url2 in url_list2:
+            img2 = download_image(url2)
+            if np.array_equal(img1, img2):
+                identical_images.append((url1, url2))
+            img2 = None
             gc.collect()
-    return similar_images
-
+        img1 = None
+        gc.collect()
+    return identical_images
 
 @app.post('/similarityCheck')
-def similarity_check(data: Similarity):
-    inp_imgs = [cv2.imdecode(np.fromstring(img, np.uint8), cv2.IMREAD_COLOR) for img in data.inpImg]
-    pro_urls = data.proImg
-
-    result = are_images_similar(inp_imgs, pro_urls)
-    output_dict = {"similarity": len(result) > 0, "similar_images": result}
+def profanityCheck(data:Similarity):
+    inpImgs = data.inpImg
+    proImgs = data.proImg
+    
+    result = are_images_identical(inpImgs, proImgs)
+    output_dict = {"identical": len(result) > 0, "identical_images": result}
     return JSONResponse(content=output_dict)
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
