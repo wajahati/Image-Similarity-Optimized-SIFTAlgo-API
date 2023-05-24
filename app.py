fb997ba8d76bba8d2163d6380e6ecf3c4f9eda76
Wajahat Ahmad <67877858+wajahati@users.noreply.github.com>
2023-05-24 22:00:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from API import Similarity
+
 import cv2
 import numpy as np
 import gc
@@ -11,25 +12,44 @@ from PIL import Image
 
 app = FastAPI()
 
-def download_image(url, max_size=800):
+def download_image(url):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    img.thumbnail((max_size, max_size), Image.ANTIALIAS)
     return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
-def are_images_identical(url_list1, url_list2):
-    identical_images = []
+def compare_images(img1, img2):
+    sift = cv2.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    good_matches = []
+
+    for m, n in matches:
+        if m.distance < 0.10*n.distance :
+          if m.distance==0:
+            good_matches.append(1)
+            # break
+    return len(good_matches)
+
+def are_images_similar(url_list1, url_list2):
+    threshold = 90
+    similar_images = []
     for url1 in url_list1:
         img1 = download_image(url1)
         for url2 in url_list2:
             img2 = download_image(url2)
-            if np.array_equal(img1, img2):
-                identical_images.append((url1, url2))
-            img2 = None
+            num_good_matches = compare_images(img1, img2)
+            if num_good_matches > threshold:
+                similar_images.append((url1, url2))
+                return similar_images
+            del img2
             gc.collect()
-        img1 = None
+        del img1
         gc.collect()
-    return identical_images
+    return similar_images
 
 @app.post('/similarityCheck')
 def profanityCheck(data:Similarity):
